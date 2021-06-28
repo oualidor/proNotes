6c652da9618fbacb676520f7e3d23665d2c4a887
oualidor <walid.khial@gmail.com>
2021-06-28 21:00:05 +0100

    Visitor Routers added

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+const visitorRouters = [
+    {
+        path: "/Landing",
+        name: "Landing",
+        component: null,
+    },
+    {
+        path: "/ArticleDetails",
+        name: "ArticleDetails",
+        component: null,
+     },
+
+];
+
+module.exports = { visitorRouters};
