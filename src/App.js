dc9d27f0be461dbfb72c0363d48c9fb76f883653
Oualid KHIAL <walid.khial@gmail.com>
2021-06-28 20:44:02 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+import React from "react";
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    async componentDidMount() {
+
+
+    }
+
+     render() {
+
+       return (
+            <React.Fragment>
+
+            </React.Fragment>
+        );
+    }
+}
+
+export default App;
